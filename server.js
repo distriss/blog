4a9d4bdca4d6ac136bce67b8669b678c02899516
Triss Marsh <marshtriss@gmail.com>
2023-07-25 14:37:53 +0100

    fix: db connection

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const PORT = process.env.PORT || 5050;
 require('dotenv').config();
 
 const app = express();
 
-// middleware
 app.use(cors());
 app.use(express.json());
+
 
 mongoose.connect(process.env.DB_STRING)
     .then(() => console.log('Connected to DB!'))
@@ -23,31 +24,27 @@ const postSchema = new mongoose.Schema({
 
 const Post = mongoose.model('Post', postSchema);
 
-
 app.get('/posts', async (req, res) => {
     const posts = await Post.find();
     res.send(posts);
 });
-
 
 app.get('/posts/:id', async (req,res) => {
     const post = await Post.findById(req.params.id);
     res.send(post);
 });
 
-// create post
 app.post('/posts', async (req, res) => {
     const newPost = new Post(req.body);
     const savedPost = await newPost.save();
     res.send(savedPost);
 });
 
-// delete post
 app.delete('/posts/:id', async (req, res) => {
     await Post.findByIdAndDelete(req.params.id);
     res.status(200).send('Post deleted.');
 });
 
-app.listen(process.env.PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 
